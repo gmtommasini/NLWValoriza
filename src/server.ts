dfa7526fad96870049449603da7271537df552ed
Guilherme Tommasini <gmtommasini@gmail.com>
2021-06-21 20:40:06 -0400

    day 2 - typeorm install

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,11 @@
+import "reflect-metadata";
 import express from "express"; //need to install types lib (yarn add @types/express)
+
+import "./database"; // by default index file is imported
 const app = express();
 
 //ROUTES
 
-app.get("/test", (req, res) =>{
-    return res.send("Hello GET!!")
-} );
-
-//BROWSER DOES NOT POST - USE POSTMAN or something else
-app.post("/test-post", (req, res)=>{
-    return res.send("Hello post");
-})
 
 app.listen(3000, ()=> console.log("Server is running NOW"));
 //listen takes a port and a function to be called
